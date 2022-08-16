5d8d66d93bb24379aca49728a50b5c8ceedc0812
Schilovich Alexander <sachilovich.alex@gmail.com>
2022-08-17 02:43:05 +0300

    added faq

--- js/components/faq.js
+++ js/components/faq.js
@@ -1,8 +1,37 @@
 const buttonFaq = document.querySelector(".button__faq");
+const buttonFaq2 = document.querySelector(".button__faq-2");
+const buttonFaq3 = document.querySelector(".button__faq-3");
+const buttonFaq4 = document.querySelector(".button__faq-4");
+const buttonFaq5 = document.querySelector(".button__faq-5");
+
 const popUpFaq = document.querySelector(".faq__block__active");
+const popUpFaq2 = document.querySelector(".faq__block__active-2");
+const popUpFaq3 = document.querySelector(".faq__block__active-3");
+const popUpFaq4 = document.querySelector(".faq__block__active-4");
+const popUpFaq5 = document.querySelector(".faq__block__active-5");
 
-buttonFaq.addEventListener("click", openPopUp1);
+buttonFaq.addEventListener("click", openPopUp);
+buttonFaq2.addEventListener("click", openPopUp2);
+buttonFaq3.addEventListener("click", openPopUp3);
+buttonFaq4.addEventListener("click", openPopUp4);
+buttonFaq5.addEventListener("click", openPopUp5);
 
-function openPopUp1() {
+function openPopUp() {
   popUpFaq.classList.toggle("faq__block__active--open");
+}
+
+function openPopUp2() {
+  popUpFaq2.classList.toggle("faq__block__active--open");
+}
+
+function openPopUp3() {
+  popUpFaq3.classList.toggle("faq__block__active--open");
+}
+
+function openPopUp4() {
+  popUpFaq4.classList.toggle("faq__block__active--open");
+}
+
+function openPopUp5() {
+  popUpFaq5.classList.toggle("faq__block__active--open");
 }
--- js/components/header.js
+++ js/components/header.js
@@ -1,26 +1,22 @@
+const menubuttonHeaderDropdown = document.querySelector(".menu__link-dropdown");
+const popUpHeaderSubscriptions = document.querySelector(".sub-menu__list");
 
-
-const menubuttonHeaderDropdown = document.querySelector('.menu__link-dropdown');
-const popUpHeaderSubscriptions = document.querySelector('.sub-menu__list');
-
-
-menubuttonHeaderDropdown.addEventListener('click', openPopUpHeaderSubscriptions);
+menubuttonHeaderDropdown.addEventListener(
+  "click",
+  openPopUpHeaderSubscriptions
+);
 
 function openPopUpHeaderSubscriptions() {
-    popUpHeaderSubscriptions.classList.toggle ('sub-menu__list--active')
+  popUpHeaderSubscriptions.classList.toggle("sub-menu__list--active");
 }
 
+const buttonHeaderDropdown = document.querySelector(
+  ".menu-language__link--dropdown"
+);
+const popUpHeaderlanguage = document.querySelector(".sub-menu-language__list");
 
-
-
-
-
-const buttonHeaderDropdown = document.querySelector('.menu-language__link--dropdown');
-const popUpHeaderlanguage = document.querySelector('.sub-menu-language__list');
-
-
-buttonHeaderDropdown.addEventListener('click', openPopUpHeaderLanguage);
+buttonHeaderDropdown.addEventListener("click", openPopUpHeaderLanguage);
 
 function openPopUpHeaderLanguage() {
-    popUpHeaderlanguage.classList.toggle ('sub-menu-language__list--active')
+  popUpHeaderlanguage.classList.toggle("sub-menu-language__list--active");
 }
