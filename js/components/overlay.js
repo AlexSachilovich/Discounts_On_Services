405e91000a5ef337c9307d5c39836deb934d1d08
Schilovich Alexander <sachilovich.alex@gmail.com>
2022-08-25 16:55:12 +0300

    finish

--- js/components/overlay.js
+++ js/components/overlay.js
@@ -1,9 +1,11 @@
 const popup = document.querySelector(".popup");
 const buttonAutorization = document.querySelector(".authorization__button");
 const buttonStart = document.querySelector(".button--start");
+const buttonMobil = document.querySelector(".button__mobil");
 const overlay = document.querySelector(".popup__overlay");
 
 buttonAutorization.addEventListener("click", showPopup);
+buttonMobil.addEventListener("click", showPopup);
 buttonStart.addEventListener("click", showPopup);
 overlay.addEventListener("click", hiddenPopup);
 
